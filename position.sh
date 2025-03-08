085c2359a72db1f366d09bf63180798405b4ea05
RightTr <543880393@qq.com>
2025-03-08 18:59:07 +0800

    3.8 main

--- position.sh
+++ position.sh
@@ -2,7 +2,10 @@
 sleep 4
 source /opt/ros/noetic/setup.bash
 
-source /home/right/livox/devel/setup.bash
+source /home/nf/Desktop/Livox/devel/setup.bash
+
+gnome-terminal -- bash -c "sudo chmod +666 /dev/ttyUSB0; exec bash"
+sleep 2
 
 gnome-terminal -- bash -c "roslaunch livox_ros_driver2 msg_MID360.launch; exec bash"
 sleep 2
@@ -10,11 +13,11 @@ sleep 2
 gnome-terminal -- bash -c "roslaunch point_lio mapping_horizon.launch; exec bash" 
 sleep 2
 
-source /home/right/depth_clustering_ws/devel/setup.bash
-gnome-terminal -- bash -c "roslaunch depth_clustering Mid360.launch; exec bash" 
-sleep 3
+source /home/nf/depth_clustering_ws/devel/setup.bash
+gnome-terminal -- bash -c "roslaunch depth_clustering Mid360.launch; exec bash"
+sleep 2
 
-source /home/right/Position/devel/setup.bash
+source /home/nf/Position_dc_jetson/devel/setup.bash
 gnome-terminal -- bash -c "rosrun position position; exec bash" 
 sleep 2
 
