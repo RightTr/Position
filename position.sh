199bb69d1847d43e0cab4d82f90b4df604e645f7
RightTr <543880393@qq.com>
2025-05-11 10:20:55 +0800

    5.11 Running on Jetson Successfully

--- position.sh
+++ position.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-sleep 4
+sleep 2
 source /opt/ros/noetic/setup.bash
 
 source /home/nf/Desktop/Livox/devel/setup.bash
 
 gnome-terminal -- bash -c "sudo chmod +666 /dev/ttyUSB0; exec bash"
-sleep 2
+sleep 1
 
 gnome-terminal -- bash -c "roslaunch livox_ros_driver2 msg_MID360.launch; exec bash"
-sleep 2
+sleep 4
 
 gnome-terminal -- bash -c "roslaunch point_lio mapping_horizon.launch; exec bash" 
-sleep 2
+sleep 1
 
 source /home/nf/depth_clustering_ws/devel/setup.bash
 gnome-terminal -- bash -c "roslaunch depth_clustering Mid360.launch; exec bash"
-sleep 2
+sleep 1
 
 source /home/nf/Position_dc_jetson/devel/setup.bash
 gnome-terminal -- bash -c "roslaunch position position.launch; exec bash" 
-sleep 2
+sleep 1
 
 echo "ROS launch file started successfully!"
